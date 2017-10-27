2537db71f019391ab3282c7beb8b5234158fd4a6
Cheney <30592850+chenguoo@users.noreply.github.com>
2017-10-27 11:26:29 +0800

    常规提交

--- java-zookeeper/src/main/java/com/cheney/study/curator/CuratorCreateSessionDemo.java
+++ java-zookeeper/src/main/java/com/cheney/study/curator/CuratorCreateSessionDemo.java
@@ -5,25 +5,21 @@ import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
 
-/**
- * 腾讯课堂搜索 咕泡学院
- * 加群获取视频：608583947
- * 风骚的Michael 老师
- */
+
 public class CuratorCreateSessionDemo {
-    public static void main(String[] args) {
-        //创建会话的两种方式 normal
-        CuratorFramework curatorFramework= CuratorFrameworkFactory.
-                newClient(ZkConstants.CONNECT_STRING,5000,5000,
-                        new ExponentialBackoffRetry(1000,3));
-        curatorFramework.start(); //start方法启动连接
+  public static void main(String[] args) {
+    //创建会话的两种方式 normal
+    CuratorFramework curatorFramework = CuratorFrameworkFactory.
+      newClient(ZkConstants.CONNECT_STRING, 5000, 5000,
+        new ExponentialBackoffRetry(1000, 3));
+    curatorFramework.start(); //start方法启动连接
 
-        //fluent风格
-        CuratorFramework curatorFramework1=CuratorFrameworkFactory.builder().connectString(ZkConstants.CONNECT_STRING).sessionTimeoutMs(5000).
-                retryPolicy(new ExponentialBackoffRetry(1000,3)).
-                namespace("/curator").build();
+    //fluent风格
+    CuratorFramework curatorFramework1 = CuratorFrameworkFactory.builder().connectString(ZkConstants.CONNECT_STRING).sessionTimeoutMs(5000).
+      retryPolicy(new ExponentialBackoffRetry(1000, 3)).
+      namespace("curator").build();
 
-        curatorFramework1.start();
-        System.out.println("success");
-    }
+    curatorFramework1.start();
+    System.out.println("success");
+  }
 }
