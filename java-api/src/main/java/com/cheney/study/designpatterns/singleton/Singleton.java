8e685a025c424ee77d7bb5f4417585e8bc7dd08f
chenguoo <chenguoo>
2017-11-16 20:27:19 +0800

    添加单利模式

---
+++ java-api/src/main/java/com/cheney/study/designpatterns/singleton/Singleton.java
@@ -0,0 +1,20 @@
+package com.cheney.study.designpatterns.singleton;
+
+/**
+ * 最简单的实现方式：
+ * 问题:不能控制何时被创建，及没有使用到实例时也会被创建（如只获取STATUS时）
+ */
+public class Singleton {
+
+    public static int STATUS = 1;
+
+    private Singleton() {
+        System.out.println("Singleton is create!");
+    }
+
+    private static Singleton instance = new Singleton();
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
