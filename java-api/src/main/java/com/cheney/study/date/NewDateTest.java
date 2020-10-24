a455f4c4a145470b6fa3a52334d00a3dad791f00
Cheney <2270292886@qq.com>
2020-10-24 10:43:23 +0800

    add  JDK8新日期

--- java-api/src/main/java/com/cheney/study/date/NewDateTest.java
+++ java-api/src/main/java/com/cheney/study/date/NewDateTest.java
@@ -1,6 +1,14 @@
 package com.cheney.study.date;
 
+import org.junit.Test;
+
 import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.Date;
 
 /**
  * todo
@@ -102,4 +110,110 @@ public class NewDateTest {
     }
 
 
+    /**设置格式化模板**/
+    private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSS");
+
+    /**设置日期时区常量**/
+    public static final ZoneId CHINA_ZONE_ID = ZoneId.systemDefault();
+    /**Date格式化为DateTime**/
+    @Test
+    public void dateToDateTime(){
+        Date date = new Date();
+        LocalDateTime dateTime = date.toInstant().atZone(CHINA_ZONE_ID).toLocalDateTime();
+        System.out.println(dateTime);
+    }
+    /**LocalDate/LocalDateTime转Date**/
+    @Test
+    public void toDate(){
+        // LocalDate
+        LocalDate localDate = LocalDate.now();
+        Date d1 = Date.from(localDate.atStartOfDay(CHINA_ZONE_ID).toInstant());
+        System.out.println(d1);
+
+        // LocalDateTime
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Date d2 = Date.from(localDateTime.atZone(CHINA_ZONE_ID).toInstant());
+        System.out.println(d2);
+    }
+
+    /**日期格式化**/
+    @Test
+    public void formatDate(){
+        System.out.println(LocalDateTime.now().format(DATE_TIME_FORMATTER));
+    }
+    /**日期加减**/
+    @Test
+    public void plusDay(){
+        LocalDateTime dateTime = LocalDateTime.now(CHINA_ZONE_ID);
+        //天
+        dateTime=dateTime.plusDays(1);
+        //时
+        dateTime=dateTime.plusHours(-1);
+        //分钟
+        dateTime=dateTime.plusMinutes(30);
+        System.out.println(dateTime.format(DATE_TIME_FORMATTER));
+    }
+    /**日期时间间隔**/
+    @Test
+    public void betweenDay(){
+        // LocalDateTime
+        LocalDateTime startDate = LocalDateTime.of(2019,07,01,12,12,22);
+        LocalDateTime endDate = LocalDateTime.of(2019,07,03,12,12,22);
+        Long withSecond =  endDate.atZone(CHINA_ZONE_ID).toEpochSecond() - startDate.atZone(CHINA_ZONE_ID).toEpochSecond();
+        System.out.println(withSecond/60/60/24);
+
+        // LocalDate
+        LocalDate startDate2 = LocalDate.of(2019,07,01);
+        LocalDate endDate2 = LocalDate.of(2019,07,03);
+        Long withSecond2 =  endDate2.toEpochDay() - startDate2.toEpochDay();
+        System.out.println(withSecond2);
+    }
+
+    /**第一天and最后一天**/
+    @Test
+    public void theLastDay(){
+        // 当月第一天
+        LocalDateTime dateTime = LocalDateTime.of(2019,07,03,12,12,22);
+        dateTime = dateTime.with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println(dateTime);
+        // 当月最后一天
+        dateTime = dateTime.with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println(dateTime);
+
+        //当月的第几天
+        dateTime = LocalDateTime.now();
+        int dayOfMonth = dateTime.getDayOfMonth();
+        System.out.println(dayOfMonth);
+        // 当前周的第几天
+        int dayOfWeek = dateTime.getDayOfWeek().getValue();
+        System.out.println(dayOfWeek);
+    }
+
+    /*
+    SpringBoot中应用LocalDateTime
+
+    将LocalDateTime字段以时间戳的方式返回给前端添加日期转化类
+    eg:
+    public class LocalDateTimeConverter extends JsonSerializer{
+        @Override
+        public void serialize(LocalDateTime value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeNumber(value.toInstant(ZoneOffset.of("+8")).toEpochMilli());
+        }
+    }
+
+    并在LocalDateTime字段上添加@JsonSerialize(using = LocalDateTimeConverter.class)注解，如下：
+    eg:
+    @JsonSerialize(using = LocalDateTimeConverter.class)
+    protected LocalDateTime gmtModified;
+
+    将LocalDateTime字段以指定格式化日期的方式返回给前端 在LocalDateTime字段上添加@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")注解即可，如下：
+    eg:
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    protected LocalDateTime gmtModified;
+
+    对前端传入的日期进行格式化 在LocalDateTime字段上添加@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")注解即可，如下：
+    eg:
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    protected LocalDateTime gmtModified;
+*/
 }
